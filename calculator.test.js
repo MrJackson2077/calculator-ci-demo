a5a1900b122f9e8e96adf2718b09d0f20f705d68
MrJackson2077 <damaniayeye@gmail.com>
2026-06-12 10:20:34 -0700

    Added more tests

--- calculator.test.js
+++ calculator.test.js
@@ -5,3 +5,21 @@ test("adds 2+3", () => {
     expect(add(2, 3)).toBe(5);
 
 });
+
+test("subtracts 2-A", () => {
+
+    expect(subtract(2, 3)).toBe(-1);
+
+});
+
+test("multiply 2*3", () => {
+
+    expect(multiply(2, 3)).toBe(6);
+
+});
+
+test("divide 2/3", () => {
+
+    expect(divide(2, 3)).toBe(2 / 3);
+
+});
