fd4b8e6c80fdb432cc543e22f6a7c53006d9bd55
MrJackson2077 <damaniayeye@gmail.com>
2026-06-12 11:58:40 -0700

    Fixed errors

--- calculator.test.js
+++ calculator.test.js
@@ -5,15 +5,3 @@ test("adds 2+3", () => {
     expect(add(2, 3)).toBe(5);
 
 });
-
-test("adds 5+10", () => {
-
-    expect(add(5, 10)).toBe(115);
-
-});
-
-test("adds 10+15", () => {
-
-    expect(add(10, 15)).toBe(0);
-
-});
